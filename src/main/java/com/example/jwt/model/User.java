c927baa34825da2aa1cd8ebfa93a15a8f8abbefe
sarang682 <jms20914@gmail.com>
2023-12-05 21:59:41 +0900

    환경 설정

---
+++ src/main/java/com/example/jwt/model/User.java
@@ -0,0 +1,31 @@
+package com.example.jwt.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String password;
+    private String roles; // USER,MANAGER,ADMIN
+
+    public List<String> getRoleList() {
+        if(this.roles.length() > 0) {
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
+
+}
